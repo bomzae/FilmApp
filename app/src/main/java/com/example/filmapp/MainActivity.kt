2cfe9d947849770dfa21396f433c125de70cce9b
DAEUN KIM <tmzl235@naver.com>
2024-01-30 10:48:14 +0900

    Login Annotation Commit

--- app/src/main/java/com/example/filmapp/MainActivity.kt
+++ app/src/main/java/com/example/filmapp/MainActivity.kt
@@ -8,10 +8,10 @@ import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    lateinit var editID: EditText
-    lateinit var editPwd: EditText
-    lateinit var loginBtn: Button
-    lateinit var registerBtn: Button
+    lateinit var editID: EditText // 아이디 입력
+    lateinit var editPwd: EditText // 패스워드 입력
+    lateinit var loginBtn: Button // 로그인 버튼
+    lateinit var registerBtn: Button // 회원가입 버튼
     var DB: DBHelper?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,13 +28,14 @@ class MainActivity : AppCompatActivity() {
             var str_id: String = editID.text.toString()
             var str_pwd: String = editPwd.text.toString()
 
+            // 두 텍스트에 빈 텍스트가 있는 경우
             if(str_id == "" || str_pwd == "") Toast.makeText(
                 this@MainActivity,
                 "회원 정보를 모두 입력해주세요",
                 Toast.LENGTH_SHORT
-            ).show() else {
-                val checkUserPass = DB!!.checkUserpass(str_id, str_pwd)
-                if (checkUserPass == true) {
+            ).show() else { // 두 텍스트 모두 입력이 있는 경우
+                val checkUserPass = DB!!.checkUserpass(str_id, str_pwd) // DB에 있는 아이디, 비밀번호인지 체크
+                if (checkUserPass == true) { // 아이디, 비밀번호가 모두 확인되면 로그인 성공
                     Toast.makeText(
                         this@MainActivity,
                         "로그인 되었습니다.",
@@ -42,7 +43,7 @@ class MainActivity : AppCompatActivity() {
                     ).show()
                     val intent = Intent(applicationContext, HomeActivity::class.java)
                     startActivity(intent)
-                } else {
+                } else { // 아이디나 비밀번호가 일치하지 않으면 로그인 실패
                     Toast.makeText(
                         this@MainActivity,
                         "회원 정보가 일치하지 않습니다.",
@@ -51,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            registerBtn.setOnClickListener {
+            registerBtn.setOnClickListener {// 회원가입 페이지로 이동
                 val loginIntent = Intent(this@MainActivity, RegisterActivity::class.java)
                 startActivity(loginIntent)
             }
